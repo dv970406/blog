12e44f0c2d8f5cacc36c81e80acf0da55d60cb24
dv970406 <dv970406@gmail.com>
2023-06-22 15:11:57 +0900

    feat: Add Skills
    - 사용할 수 있는 기술 스택 보여주기

---
+++ frontend/src/components/templates/home/Skills.tsx
@@ -0,0 +1,37 @@
+import Title from "@/components/molecules/Title";
+import Image from "next/image";
+import React from "react";
+
+const LOGOS = [
+  "React",
+  "Typescript",
+  "NextJS",
+  "React-Query",
+  "GraphQL",
+  "Relay",
+  "Styled-Components",
+  "TailwindCSS",
+];
+const Skills = () => {
+  return (
+    <section className="h-screen py-4">
+      <Title title="SKILLS" />
+
+      <ul className="grid grid-cols-3 gap-4 mt-10">
+        {LOGOS.map((logo) => (
+          <li key={logo} className="flex flex-col items-center">
+            <Image
+              src={`/skills/${logo}.svg`}
+              width={64}
+              height={64}
+              alt="react"
+            />
+            <p className="text-center text-main">{logo}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Skills;
