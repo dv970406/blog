d849543b873bda6af60d0bbf5da5d236efaf502e
dv970406 <dv970406@gmail.com>
2023-06-22 15:09:20 +0900

    feat: Add Projects
    - 내 프로젝트 리스트 보여주기

---
+++ frontend/src/components/organisms/home/Project.tsx
@@ -0,0 +1,104 @@
+"use client";
+import { useIntersectionObserver } from "@/hooks/IntersectionObserver";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useRef, useState } from "react";
+
+interface IProject {
+  isOdd: boolean;
+  title: string;
+  thumbnail: string;
+  github: string;
+  link: string;
+  review: string;
+  priority: boolean;
+  description: string;
+  stacks: string[];
+}
+const Project = ({
+  isOdd,
+  title,
+  thumbnail,
+  github,
+  link,
+  review,
+  priority,
+  description,
+  stacks,
+}: IProject) => {
+  const projectRef = useRef<HTMLLIElement>(null);
+
+  const { isIntersectProject } = useIntersectionObserver(projectRef);
+  return (
+    <li
+      ref={projectRef}
+      key={title}
+      className={`flex flex-col items-center gap-4 lg:justify-between lg:gap-20 lg:flex-row  opacity-0
+       ${isIntersectProject && "animate-appear-bottom-to-top"}
+      `}
+    >
+      <Image
+        src={`/thumbnails/${thumbnail}`}
+        className={`${isOdd && "lg:order-1"} rounded-lg lg:w-1/2 `}
+        alt={title}
+        width={600}
+        height={600}
+        priority={priority}
+      />
+      <div className={`flex flex-col justify-between lg:w-1/2 gap-6 `}>
+        <h2
+          className={`tracking-wider uppercase text-gradient text-title ${
+            isOdd ? "md:text-left" : "md:text-right"
+          }`}
+        >
+          {title}
+        </h2>
+        <div className="flex flex-col gap-6">
+          <p className="break-words text-sub">{description}</p>
+          <ul
+            className={`flex flex-wrap gap-2 ${!isOdd && "md:place-self-end"}`}
+          >
+            {stacks.map((stack) => (
+              <li
+                key={stack}
+                className="py-1 px-2 border-[1px] rounded-lg border-custom-lightgray text-cute"
+              >
+                {stack}
+              </li>
+            ))}
+          </ul>
+          <ul className={`flex gap-6 ${!isOdd && "md:place-self-end"}`}>
+            {github && (
+              <li>
+                <Link target="_blank" href={github}>
+                  <Image
+                    src={"/Github.png"}
+                    width={25}
+                    height={25}
+                    alt={title}
+                  />
+                </Link>
+              </li>
+            )}
+            {link && (
+              <li>
+                <Link target="_blank" href={link}>
+                  <Image src={"/Link.png"} width={25} height={25} alt={title} />
+                </Link>
+              </li>
+            )}
+            {review && (
+              <li>
+                <Link target="_blank" href={review}>
+                  <Image src={"/Bulb.png"} width={25} height={25} alt={title} />
+                </Link>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default Project;
---
+++ frontend/src/hooks/IntersectionObserver.ts
@@ -0,0 +1,30 @@
+"use client";
+
+import { RefObject, useEffect, useState } from "react";
+
+export const useIntersectionObserver = (
+  projectRef: RefObject<HTMLLIElement>
+) => {
+  const [isIntersectProject, setIsIntersectProject] = useState(false);
+
+  useEffect(() => {
+    if (!projectRef.current) return;
+    let observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) setIsIntersectProject(true);
+        });
+      },
+      { threshold: 0.02 }
+    );
+
+    observer.observe(projectRef?.current);
+
+    return () => {
+      if (!projectRef.current) return;
+      observer.unobserve(projectRef?.current);
+    };
+  }, []);
+
+  return { isIntersectProject };
+};
