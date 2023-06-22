d0bc1075a73c417fee06d643d21d9d208a6a4cfc
dv970406 <dv970406@gmail.com>
2023-06-22 15:15:43 +0900

    feat: Add Pretendard 폰트 추가 및 metadata작성

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Contact from "@/components/templates/home/Contact";
 import Information from "@/components/templates/home/Information";
 import Projects from "@/components/templates/home/Projects";
+import Skills from "@/components/templates/home/Skills";
 
 export default function Home() {
   return (
     <>
       <Information />
       <Projects />
+      <Skills />
       <Contact />
     </>
   );
