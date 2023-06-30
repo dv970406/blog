5f1d2d2368b17c6136d526f1901dcde39897d846
dv970406 <dv970406@gmail.com>
2023-06-30 16:27:16 +0900

    fix: Modify Textarea Title
    - Textarea 부분 SubTitle이 하드코딩 되어있던 것을 변경

--- frontend/src/components/molecules/Textarea.tsx
+++ frontend/src/components/molecules/Textarea.tsx
@@ -3,11 +3,12 @@ import React, { TextareaHTMLAttributes, LegacyRef } from "react";
 
 interface ITextarea extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   textareaRef: LegacyRef<HTMLTextAreaElement>;
+  title: string;
 }
-const Textarea = ({ textareaRef }: ITextarea) => {
+const Textarea = ({ textareaRef, title }: ITextarea) => {
   return (
     <>
-      <SubTitle title="Name" />
+      <SubTitle title={title} />
       <textarea
         ref={textareaRef}
         className="w-full h-48 p-2 rounded-lg outline-none bg-custom-black ring-transition"
